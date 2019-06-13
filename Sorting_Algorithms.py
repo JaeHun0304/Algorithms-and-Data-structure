0017d740294616f71066d474fe152259497a2d98
jaehun <jjh44723@gmail.com>
2019-06-13 01:30:02 -0500

    modified:   Sorting_Algorithms.py

--- Sorting_Algorithms.py
+++ Sorting_Algorithms.py
@@ -1,14 +1,25 @@
 """" Various Sorting Algorithms will be implemented here in Python """
-
+import pdb
 
 def insertionsort(numbers):
-	"""" Sort passing numbers with insertion sort """
+	"""" Sort passing numbers with insertion sort with non decreasing order """
 	for number in range(1, len(numbers)):
 		key = numbers[number]
 		index = number - 1
-		while index > 0 and numbers[index] > key:
+		while index >= 0 and numbers[index] > key:
 			numbers[index + 1] = numbers[index]
-			index = index - 1;
+			index = index - 1
+		numbers[index + 1] = key
+	return numbers
+
+def reverse_insertionsort(numbers):
+	""" Sort passing number lists with insertion sort and non increasing order """
+	for number in range(1, len(numbers)):
+		key = numbers[number]
+		index = number - 1
+		while index >= 0 and numbers[index] < key:
+			numbers[index + 1] = numbers[index]
+			index = index - 1
 		numbers[index + 1] = key
 	return numbers
 
@@ -16,4 +27,6 @@ def insertionsort(numbers):
 test_list = [1, 6, 3, 2, 5, 9]
 print(test_list)
 insertionsort(test_list)
+print(test_list)
+reverse_insertionsort(test_list)
 print(test_list)
